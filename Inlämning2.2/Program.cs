065b37676923cd199095d39a6634df2d004a8b32
Airblow <143172177+Airblow@users.noreply.github.com>
2023-09-15 12:41:57 +0200

    Add project files.

---
+++ Inlämning2.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace inlämning2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt i meter hoppade Elin?");
+            string elinlt = Console.ReadLine();
+            double elinln = double.Parse(elinlt);
+            Console.WriteLine("-----------------------------------");
+
+            Console.WriteLine("Hur långt i meter hoppade Alma?");
+            string almalt = Console.ReadLine();
+            double almaln = double.Parse(almalt);
+            Console.WriteLine("-----------------------------------");
+
+            double resultat =  Math.Round(elinln - almaln, 3);
+            Console.WriteLine("Elin hoppade " + resultat + " meter längre än vad Alma gjorde.");
+            Console.ReadKey();
+            
+
+        }
+
+    }
+
+}
